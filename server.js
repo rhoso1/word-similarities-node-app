5464e4f9d5977aa06aed2ab2e65e29b5cc07fd96
Ramiza Hošo <ramiza.hoso@gmail.com>
2023-10-18 13:30:22 +0200

    Adjusted server.js for default route

--- server.js
+++ server.js
@@ -9,15 +9,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var corsOptions = {
-  origin: ["http://localhost:4200", "http://localhost:8080", 'https://rhoso1.github.io/word-similarities-angular-app/search-words'],
+  origin: [
+    "http://localhost:4200",
+    "http://localhost:8080",
+    "https://rhoso1.github.io/word-similarities-angular-app",
+  ],
 };
 
 app.use(cors(corsOptions));
 
 app.use("/api/wordpairs", wordPairRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to the Word Similarities API");
+});
 
 //Disabling favicon icon
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get("/favicon.ico", (req, res) => res.status(204));
 
 const PORT = process.env.PORT || 8080;
 
